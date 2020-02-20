6fbc57a2d73508c56876ccc9ab04bcd9f74d5c2e
yoneya1014 <yoneya1014@gmail.com>
2020-02-20 16:40:28 +0900

    男子・女子寮総代挨拶の登録・編集ページの追加

--- routes/admin/managecontent/manageGreeting.js
+++ routes/admin/managecontent/manageGreeting.js
@@ -24,6 +24,7 @@ router.get('/', signInCheck, (req, res) => {
     res.render('admin/managecontent/greeting/manageGreeting');
 });
 
+//寮務主事挨拶の編集ページ用ルーティング
 router.get('/secretary', csrfProtection, signInCheck, (req, res) => {
     greetings.findOne({
         person_type: "secretary"
@@ -33,17 +34,25 @@ router.get('/secretary', csrfProtection, signInCheck, (req, res) => {
             req.session.error = true;
             res.redirect('/admin/error');
         }
-        res.render('admin/managecontent/greeting/editGreeting', {
-            _csrf: req.csrfToken(),
-            person_type: data.person_type,
-            greeting_text: data.greeting_text,
-            image_ext: data.image_ext
-        });
+        if (data == null) {
+            res.render('admin/managecontent/greeting/editGreeting', {
+                _csrf: req.csrfToken(),
+                person_type: 'secretary',
+                greeting_text: '',
+                image_ext: ''
+            });
+        } else {
+            res.render('admin/managecontent/greeting/editGreeting', {
+                _csrf: req.csrfToken(),
+                person_type: data.person_type,
+                greeting_text: data.greeting_text,
+                image_ext: data.image_ext
+            });
+        }
     });
 });
 
 router.post('/secretary', parseForm, csrfProtection, signInCheck, (req, res) => {
-    console.log(req);
     const greetingText = req.body.greeting_text;
     let imgExt = '';
     if (req.files !== null) {
@@ -57,23 +66,124 @@ router.post('/secretary', parseForm, csrfProtection, signInCheck, (req, res) => 
             if (err) {
                 console.log(err);
                 req.session.error = true;
-                res.redirect('/admin/error')
+                res.redirect('/admin/error');
             }
         })
     }
+    if (req.body.updateflag === 'false') {
+        const model = new greetings({
+            person_type: 'secretary',
+            greeting_text: greetingText,
+            image_ext: imgExt
+        });
+        model.save((err) => {
+            if (err) {
+                console.log(err);
+                req.session.error = true;
+                res.redirect('/admin/error');
+            }
+            res.redirect('/admin/managecontent/greeting');
+        });
+    } else if (req.body.updateflag === 'true') {
+        greetings.update({
+            person_type: 'secretary'
+        }, {
+            person_type: 'secretary',
+            greeting_text: greetingText,
+            image_ext: imgExt
+        }, (err) => {
+            if (err) {
+                console.log(err);
+                req.session.error = true;
+                res.redirect('/admin/error');
+            }
+            res.redirect('/admin/managecontent/greeting');
+        });
+    } else {
+        req.session.error = true;
+        res.redirect('/admin/error');
+    }
+});
 
-    greetings.update({
-        person_type: 'secretary',
-        greeting_text: greetingText,
-        image_ext: imgExt
-    }, (err) => {
+router.get('/representative', csrfProtection, signInCheck, (req, res) => {
+    greetings.findOne({
+        person_type: req.query.gender
+    }, (err, data) => {
         if (err) {
             console.log(err);
             req.session.error = true;
             res.redirect('/admin/error');
         }
-        res.redirect('/admin/managecontent/greeting');
+        if (data == null) {
+            res.render('admin/managecontent/greeting/editGreeting', {
+                _csrf: req.csrfToken(),
+                person_type: req.query.gender,
+                greeting_text: '',
+                image_ext: ''
+            });
+        } else {
+            res.render('admin/managecontent/greeting/editGreeting', {
+                _csrf: req.csrfToken(),
+                person_type: data.person_type,
+                greeting_text: data.greeting_text,
+                image_ext: data.image_ext
+            });
+        }
     });
+});
+
+router.post('/representative', parseForm, csrfProtection, signInCheck, (req, res) => {
+    const greetingText = req.body.greeting_text;
+    let imgExt = '';
+    if (req.files !== null) {
+        const savePath = "public/images/greetings/";
+        if (!fs.existsSync(savePath)) {
+            fs.mkdirSync(savePath);
+        }
+        imgExt = path.extname(req.files.imgfiles.name);
+        const newImgName = 'representative_' + req.body.person_type + imgExt;
+        fs.writeFile(savePath + newImgName, req.files.imgfiles.data, (err) => {
+            if (err) {
+                console.log(err);
+                req.session.error = true;
+                res.redirect('/admin/error');
+            }
+        })
+    }
+    if (req.body.updateflag === 'false') {
+        const model = new greetings({
+            person_type: req.body.person_type,
+            greeting_text: greetingText,
+            image_ext: imgExt
+        });
+        model.save((err) => {
+            if (err) {
+                console.log(err);
+                req.session.error = true;
+                res.redirect('/admin/error');
+            }
+            res.redirect('/admin/managecontent/greeting');
+        });
+    } else if (req.body.updateflag === 'true') {
+        greetings.update({
+            person_type: req.body.person_type
+        }, {
+            person_type: req.body.person_type,
+            greeting_text: greetingText,
+            image_ext: imgExt
+        }, (err) => {
+            if (err) {
+                console.log(err);
+                req.session.error = true;
+                res.redirect('/admin/error');
+            }
+            res.redirect('/admin/managecontent/greeting');
+        });
+    } else {
+        req.session.error = true;
+        res.redirect('/admin/error');
+    }
+
 });
 
 module.exports = router;
